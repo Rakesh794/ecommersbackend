c74e1c2856ba21b8136de078ecdf4c4be29b90d4
Rakesh794 <2400032966@kluniversity.in>
2026-03-25 10:20:47 +0530

    first commit

---
+++ src/main/java/com/klu/EcommerceeApplication.java
@@ -0,0 +1,15 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceeApplication.class, args);
+		System.out.println("Application has been running..");
+	}
+
+}
+
